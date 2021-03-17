8792612cfaed6792c35fa61c43f78b5ac240a7d7
Maxwell <maxwell.guru.1@gmail.com>
2021-03-16 18:24:05 -0600

    Initial commit

---
+++ src/com/maxwell/Main.java
@@ -0,0 +1,14 @@
+package com.maxwell;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Create SCanner
+        Scanner input = new Scanner(System.in);
+    //Prompt USer in
+    //if else statements
+    //show results
+    }
+}
